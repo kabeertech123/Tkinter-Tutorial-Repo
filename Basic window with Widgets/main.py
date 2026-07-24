fc932e622e2dff254aaf45bd36b4518cc335f3f3
Kabeer Warsi <kwarsi541@gmail.com>
2026-07-24 11:41:09 +0100

    Learnt about Lambda and completed a very basic exersice
    Learned how lambda can be used in the command keyword in tkinter

--- Basic window with Widgets/main.py
+++ Basic window with Widgets/main.py
@@ -6,8 +6,17 @@ window = tk.Tk()
 window.title('Window and Widets')
 window.geometry('800x500')
 
+#list of all the functions
+
+def button_func():
+    print('the btn was pressed')
+    
+def print_hello():
+    print('hello')
+
 
 # 👇 This makes the window always on top (until you remove it)
+
 window.attributes('-topmost', 1)
 
 # ttk widgets
@@ -23,6 +32,27 @@ text.pack()#places it in the middle top of the window
 
 entry = ttk.Entry(master=window)
 entry.pack()
+
+new_text = ttk.Label(master=window, text='my label')
+new_text.pack()
+
+hello_button = ttk.Button(master=window, text="btn", command=print_hello)
+hello_button.pack()
+
+#ttk btn
+
+#btn = ttk.Button (master=window, text='A button', command=button_func) 
+btn = ttk.Button (master=window, text='A button', command=lambda : print('hello')) 
+btn.pack()
+
+
+
+#Exercise 
+# add one more text label and a button with a function that prints 'hello'
+#The label should say my label and be betwwen the entry widget an the button
+
+
+
 
 # run 
 window.mainloop() # updates GUI and checks for events 
