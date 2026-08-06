3dffa0509b2a040dd5f64fb7df190cc72442ee89
Kabeer Warsi <kwarsi541@gmail.com>
2026-08-06 21:56:28 +0100

    Created a simple checkbox

--- 04-Buttons/main.py
+++ 04-Buttons/main.py
@@ -17,5 +17,12 @@ button_string = tk.StringVar(value='A button with string var')
 btn = ttk.Button(window, text='A simple button', command=lambda:print('a basic button'), textvariable=button_string)
 btn.pack()
 
+#checkbutton
+
+check = ttk.Checkbutton(window, text='checkbox 1')
+check.pack()
+
+#1:11:35
+
 #run
 window.mainloop()
